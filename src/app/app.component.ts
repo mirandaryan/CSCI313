e484f69fb0bb8b57d54de54570449face4f259cd
mirandaryan <miraryan21@gmail.com>
2022-03-10 19:59:21 -0600

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-import {COURSES} from '../db-data';
+import { COURSES } from '../db-data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
+  csci160 = COURSES[0];
+  csci161 = COURSES[1];
+  csci213 = COURSES[2];
+  csci313 = COURSES[3];
 }
